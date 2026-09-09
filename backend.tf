37ea57e677c527a88a17b22bdcc1db4d36d93ffb
grayburnd <grayburnd@yahoo.co.uk>
2026-09-09 15:31:24 +0100

    changing tf vars pull to external repo

--- backend.tf
+++ backend.tf
@@ -1,8 +1,3 @@
 terraform {
-  backend "s3" {
-    # bucket       = "tf-state-AWS_ACCOUNT_ID_REDACTED"
-    # key          = "aws"
-    # region       = "us-east-1"
-    # use_lockfile = true
-  }
+  backend "s3" {}
 }
